d07819943acdd0d35ccb1b5f48820100ffce412a
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-02-13 15:09:29 -0500

    Treat @boss like an Employee instance in the Employee class

--- employee.rb
+++ employee.rb
@@ -1,8 +1,12 @@
 class Employee
 
-    attr_reader :name, :title, :salary, :boss
+    attr_reader :name, :title, :salary 
 
     def initialize(name, title, salary, boss)
         @name, @title, @salary, @boss = name, title, salary, boss
+    end
+
+    def boss
+        @boss
     end
 end
