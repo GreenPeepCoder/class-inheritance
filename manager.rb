24b7bcef44d9296dcad0970f3a49482c3b6a8b8f
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-02-13 15:04:16 -0500

    Update #initialize methodS - Add #add_suborinates(*subordinates) method

--- manager.rb
+++ manager.rb
@@ -1,5 +1,20 @@
 require_relative "employee"
 
 class Manager < Employee
-    
+
+    attr_reader :subordinates
+
+    def initialize(name, title, salary, boss, subordinates)
+        super(name, title, salary, boss)
+        @subordinates = subordinates
+    end
+
+    def initialize(name, title, salary, boss)
+        super(name, title, salary, boss)
+        @subordinates = Array.new()
+    end
+
+    def add_subordinates(*subordinates)
+        subordinates.each {|sub| @subordinates << sub}
+    end
 end
